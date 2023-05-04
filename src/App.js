388eb5ebdf8d02ccc3a231a0cf634d2f8805d655
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-05-04 15:16:39 -0300

    Funcionando los 3 llamados

--- src/App.js
+++ src/App.js
@@ -8,32 +8,7 @@ import InputMap from './components/InputMap';
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ29uYWxtaXJvbiIsImEiOiJjbGd6c3BvemcwbWdxM25wOTB0amJleGFpIn0.FYeIxKL5jo5B6HtGK1XAHQ';
  
 export default function App() {
-// const mapContainer = useRef(null);
-// const map = useRef(null);
-// const [lng, setLng] = useState(-58.381849956776435);
-// const [lat, setLat] = useState(-34.603822153083456);
-// const [zoom, setZoom] = useState(9);
 
-
-// useEffect(() => {
-// if (map.current) return; // initialize map only once
-// map.current = new mapboxgl.Map({
-// container: mapContainer.current,
-// style: 'mapbox://styles/mapbox/streets-v12',
-// center: [lng, lat],
-// zoom: zoom
-// });
-// });
- 
-// useEffect(() => {
-// if (!map.current) return; // wait for map to initialize
-// map.current.on('move', () => {
-// setLng(map.current.getCenter().lng.toFixed(4));
-// setLat(map.current.getCenter().lat.toFixed(4));
-// setZoom(map.current.getZoom().toFixed(2));
-// });
-// });
- 
 return (
     <BrowserRouter>
         <div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,76 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 
 const LoginIG =  () => {
 
-  window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '231784736196912',
-        cookie     : true,
-        xfbml      : true,
-        version    : '16.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-   const redirectUri = encodeURIComponent('https://quedeporte.com.ar/auth/');
+    // EN PRODUCCION SERIA: const redirectUri = encodeURIComponent('https://quedeporte.com.ar/auth/');
+    const redirectUri = encodeURIComponent('https://localhost:443/api/auth/code');
     const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=180895391557997&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+    // const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=180895391557997&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`
 
 
+    // FUNCION PARA PEDIR DATOS DE USUARIO FUNCIONANDO, CON EL ACCESS TOKEN DE LA INTERFACE API GRAPH CON ACCESO DE USUARIO
+    const getDatos = async () => {
+        const response = await axios.get('https://graph.facebook.com/v16.0/me', {
+            params: {
+                'fields': 'name, email',
+                'access_token': 'EAADSzpbdiTABALAp0gMhH7CBIsTYI7bo3s0nNbFtuA52PjPFE6hi3dxYv2tnDyNo6p1B0skRlROZAnkWXREe0HvhZAWKsjWtYUDTNFOt8QyrrTClXcaatI3eAUlEGOZCCEZA9ncgtfojnAYMB5EZA3quzFsBInmjeaKQwP6bxXOW4grV3ACXvDRS4A63nM2JZCLMyrZCUhfBAZDZD'
+            }
+            });
+            console.log(response.data)
+    }
 
+    const getDatosApi = async () => {
+        const response = await axios.get('')
+        console.log(response.data)
+    }
 
+    // const getDatos = async () => {
+    //     const response = await axios.get('https://graph.facebook.com/v16.0/me', {
+    //         params: {
+    //             'fields': 'name,birthday,email',
+    //             'access_token': 'EAADSzpbdiTABAEthkhN9N3yfZBUqZBY8gZADsLGwVZB9bY7ZCBCM0m9TvVZCiKkM6r8001O3prnxxlkUuDD8XY9ZCNvyBMQHjv9wQfVSZBLxOB5slMNCNRA13yrrdFM3Dd7Jr3EOgqzOsnENaSL3qh8qGeT8z8RHEmU8Jw5CzMosJjuL7uoCSlZCxgiHV0RmatyGPTptpbFe4DS3RVXSrpOwdyWgBmNILvVM1bibRUed6N9XwVNeOBZAla'
+    //         }
+    //         });
+    //         console.log(response.data)
+    // }
+
+    const getAccessToken = async () => {
+        
+        const response = await axios.post(
+          'https://localhost:443/api/auth/token')
+        console.log(response.data)
+
+        }
 
     return (
     <>
     <div className="container mx-2">
         <div className="container my-2">
-            <Link to="https://localhost:443/auth/facebook"> <h1>Logear con Instagram </h1> </Link>
+            <Link to="https://localhost:443/auth/instagram"> <h1>Logear con Instagram </h1> </Link>
         </div>
         <hr/>
         <div className="container my-2">
             <Link to="https://localhost:443/auth/facebook"> <h1> Logear con Facebook </h1> </Link>
         </div>
-        <div className="fb-login-button" scope="public_profile" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div>
         </div>
 
         <div className="container my-2">
-            <a href={instagramAuthUrl}> VENTANA DE AUTORIZACION INSTAGRAM</a>
+            <button><Link to={instagramAuthUrl}> VENTANA DE AUTORIZACION INSTAGRAM</Link> </button>
         </div>
+
+
+        <div className="container my-2">
+            <button onClick={getAccessToken}> GENERAR ACCESS TOKEN CON EL CODIGO QUE NOS AUTORIZO INSTAGRAM</button>
+        </div>
+
+        <div className="container my-2">
+            <button onClick={getDatos} >PEDIR DATOS CON ACCESS TOKEN DE LA API GRAPH</button>
+        </div>
+
 
     </>
     )
